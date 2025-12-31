352f77a6df90c16c3808eda773166c9b36a89a9b
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-31 18:56:17 +0100

    code refactor

--- Day02.swift
+++ Day02.swift
@@ -2,20 +2,19 @@ import Foundation
 
 let input = try! String(contentsOfFile: "Day02.txt", encoding: .utf8)
 
-let ranges = input.split(separator: ",").map { $0.split(separator: "-").map { Int($0)! } }
-
-func isValidId(_ id: String) -> Bool {
+func isInvalidId(_ id: String) -> Bool {
   let midpoint = id.index(id.startIndex, offsetBy: id.count / 2)
   return id[..<midpoint] == id[midpoint...]
 }
 
-var count = 0
-for range in ranges {
-  for i in range[0]..<range[1] {
-    if isValidId(String(i)) {
-      count += i
-    }
-  }
-}
+let ranges = input.split(separator: ",")
+  .map { $0.split(separator: "-").map { Int($0)! } }
+  .map { $0[0]...$0[1] }
+
+let count =
+  ranges
+  .flatMap { $0 }
+  .filter { isInvalidId(String($0)) }
+  .reduce(0, +)
 
 print("Part 1: \(count)")
