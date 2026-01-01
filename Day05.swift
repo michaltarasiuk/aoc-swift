76b21c689ffcdd30c4662418dfd3d1e8a7826316
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-01-01 01:07:50 +0100

    day 5 part 1

--- Day05.swift
+++ Day05.swift
@@ -2,10 +2,10 @@ import Foundation
 
 let input = try! String(contentsOfFile: "Day05.txt", encoding: .utf8)
 
-func parseRange(_ range: String) -> (Int, Int)? {
+func parseRange(_ range: String) -> (start: Int, end: Int)? {
   let parts = range.split(separator: "-").compactMap { Int($0) }
   guard parts.count == 2 else { return nil }
-  return (parts[0], parts[1])
+  return (start: parts[0], end: parts[1])
 }
 
 let paragraphs =
@@ -17,9 +17,7 @@ let ranges = (paragraphs.first ?? []).compactMap { parseRange($0) }
 let ids = (paragraphs.last ?? []).compactMap { Int($0) }
 
 let validIds = ids.filter { id in
-  ranges.contains { range in
-    id >= range.0 && id <= range.1
-  }
+  ranges.contains { $0.start...$0.end ~= id }
 }
 
 print("Part 1: \(validIds.count)")
