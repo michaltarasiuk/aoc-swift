d86b36bacd20cfd3205bb1b095c7841e7161f2d3
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-01-22 13:27:06 +0100

    day 03, part 2 of 2024

--- Year24/03.swift
+++ Year24/03.swift
@@ -1,15 +1,31 @@
 import Foundation
+import RegexBuilder
 
 let input = try String(contentsOfFile: "03.txt", encoding: .utf8)
 
-let multiplyRegex = #/mul\((\d+),(\d+)\)/#
-
-var multiplications = 0
-for match in input.matches(of: multiplyRegex) {
-  let (_, x, y) = match.output
-  if let i = Int(x), let j = Int(y) {
-    multiplications += i * j
+let instructionRegex = Regex {
+  ChoiceOf {
+    #/mul\((\d+),(\d+)\)/#
+    #/do\(\)/#
+    #/don't\(\)/#
   }
 }
 
-print("Part 1: \(multiplications)")
+var part1Sum = 0
+var part2Sum = 0
+var enabled = true
+for match in input.matches(of: instructionRegex) {
+  let (command, x, y) = match.output
+  if command.starts(with: "mul"), let i = Int(x!), let j = Int(y!) {
+    let product = i * j
+    part1Sum += product
+    if enabled {
+      part2Sum += product
+    }
+  } else {
+    enabled = (command == "do()")
+  }
+}
+
+print("Part 1: \(part1Sum)")
+print("Part 2: \(part2Sum)")
