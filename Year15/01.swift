ff91e34dc78e24e9af76ffe7c8bab05757893194
michal <69385846+michaltarasiuk@users.noreply.github.com>
2026-01-22 01:41:18 +0100

    Update 01.swift
    Commit from phone

--- Year15/01.swift
+++ Year15/01.swift
@@ -5,7 +5,7 @@ let input = try String(contentsOfFile: "01.txt", encoding: .utf8)
 var floor = 0
 var basementEntry: Int?
 for (i, char) in input.enumerated() {
-  floor += (1) * (char == "(" ? 1 : -1)
+  floor += (char == "(" ? 1 : -1)
   if floor < 0 {
     basementEntry = basementEntry ?? (i + 1)
   }
